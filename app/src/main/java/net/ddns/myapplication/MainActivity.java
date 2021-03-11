8f406bab2ef052cfafa65918b892e5f38aacdc98
yline0808 <yline0088@gmail.com>
2021-03-12 00:36:29 +0900

    [수정] dp to px 수정중

--- app/src/main/java/net/ddns/myapplication/MainActivity.java
+++ app/src/main/java/net/ddns/myapplication/MainActivity.java
@@ -2,7 +2,10 @@ package net.ddns.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
 import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,5 +35,49 @@ public class MainActivity extends AppCompatActivity {
         ts3.setContent(R.id.tab3);
         ts3.setIndicator("tab 3");
         tabhost.addTab(ts3);
+
+        for(int i = 0; i < tabhost.getTabWidget().getChildCount(); i++){
+            tabhost.getTabWidget().getChildAt(i).getLayoutParams().height = 50;
+        }
+        Log.v("tttt", Float.toString(dpToPx(50)) );
+
     }
+    public int dpToPx(int sizeInDP){
+        int pxVal = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, sizeInDP, getResources(), getDisplayMetrics()
+        );
+        return pxVal;
+    }
+
+    //dp를 px로 변환 (dp를 입력받아 px을 리턴)
+    public float convertDpToPixel(float dp){
+        Resources resources = this.getResources();
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        float px = dp * ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+        return px;
+    }
+
+    //px을 dp로 변환 (px을 입력받아 dp를 리턴)
+    public float convertPixelsToDp(float px){
+        Resources resources = this.getResources();
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        float dp = px / ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+        return dp;
+    }
+
+//    //dp를 px로 변환 (dp를 입력받아 px을 리턴)
+//    public static float convertDpToPixel(float dp, Context context){
+//        Resources resources = context.getResources();
+//        DisplayMetrics metrics = resources.getDisplayMetrics();
+//        float px = dp * ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+//        return px;
+//    }
+//
+//    //px을 dp로 변환 (px을 입력받아 dp를 리턴)
+//    public static float convertPixelsToDp(float px, Context context){
+//        Resources resources = context.getResources();
+//        DisplayMetrics metrics = resources.getDisplayMetrics();
+//        float dp = px / ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+//        return dp;
+//    }
 }
