cfcdb181a981e35d1511b3793951fd6143b43c70
류요선 <ryuyoseon@rysMAC.local>
2021-07-21 20:43:18 +0900

    modify xml waring
    * _activity_set_alarm text&description strings.xml로 이동
    * activity_main text&description strings.xml로 이동
    * activity_main 버그 수정

---
+++ app/src/main/java/net/ddns/yline/alarm/SetAlarmActivity.kt
@@ -0,0 +1,11 @@
+package net.ddns.yline.alarm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SetAlarmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_alarm)
+    }
+}
--- app/src/main/java/net/ddns/yline/alarm/fragment/NormalAlarmListFragment.kt
+++ app/src/main/java/net/ddns/yline/alarm/fragment/NormalAlarmListFragment.kt
@@ -1,13 +1,17 @@
 package net.ddns.yline.alarm.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import net.ddns.yline.alarm.R
+import net.ddns.yline.alarm.SetAlarmActivity
 import net.ddns.yline.alarm.databinding.TabNormalAlarmListBinding
 
 class NormalAlarmListFragment: Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,7 +22,16 @@ class NormalAlarmListFragment: Fragment() {
             container,
             false
         ).apply {
-
+            imgbtnNormalAlarmAdd.setOnClickListener {  }
         }.root
+    }
+
+    inner class btnClickListener:View.OnClickListener{
+        override fun onClick(v: View?) {
+            when (v?.id){
+
+            }
+            val intent = Intent(context, SetAlarmActivity::class.java)
+        }
     }
 }
